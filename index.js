efeb91d44183ed829df479cadc81e46e30d084e3
Mahek-Kariya <mahekkariya88@gmail.com>
2024-08-24 18:32:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function playGame() {
+  var randomNumber1 = Math.floor((Math.random()*6)+1);
+  var image1 = "./images/dice" + randomNumber1 +".png";
+  document.querySelector(".img1").setAttribute("src",image1);
+  var randomNumber2 = Math.floor((Math.random()*6)+1);
+  var image2 = "./images/dice" + randomNumber2 +".png";
+  document.querySelector(".img2").setAttribute("src",image2);
+  if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player1 wins";
+  } else if(randomNumber1==randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw";
+  } else{
+    document.querySelector("h1").innerHTML = "Player2 wins";
+  }
+}
+
